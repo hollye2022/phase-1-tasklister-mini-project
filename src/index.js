de40d3b905c4f492f88042cee0aa4f852175c6cc
chengying <hollye2558@gmail.com>
2022-11-15 20:55:38 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+   const taskList = document.getElementById("tasks")   
+   const li = document.createElement("li");
+   li.textContent = e.target["new-task-description"].value;
+   
+   const btn = document.createElement("button");
+   btn.textContent = "x";
+   taskList.append(li , btn);
+
+   btn.addEventListener("click", ()=> {
+    btn.remove();
+    li.remove();
+   })
+   
+  
+   // task.appendChild(e.target["new-task-description"].value);
+  })
+  
 });
+
